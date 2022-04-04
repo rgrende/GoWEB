c848af81c05a4f627f4c9a53b943a00895aed229
Rakiah Grende <rgrende@carroll.edu>
2022-04-04 14:09:03 -0600

    Adding GOWEB files

---
+++ src/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	router := gin.Default()
+	router.Static("/static", "./static")
+	router.LoadHTMLGlob("templates/*")
+	router.GET("/home", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "home.tmpl", gin.H{
+			"title":   "Jukebox-3000",
+			"heading": "Search for albums in the links below!",
+			"link1":   "Best of the 80s",
+			"link2":   "Easy Listening",
+			"image":   "/static/img/jukebox.png",
+		})
+	})
+	router.GET("/eighties", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "eighties.tmpl", gin.H{
+			"title":   "Jukebox-3000",
+			"heading": "Best of the 80s",
+			"link3":   "Home",
+		})
+	})
+	router.GET("/easy", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "easy.tmpl", gin.H{
+			"title":   "Jukebox-3000",
+			"heading": "Easy Listening",
+			"link3":   "Home",
+		})
+	})
+
+	router.Run(":8080")
+}
